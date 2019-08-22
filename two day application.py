00c421260159c122d9a438294d131196ee7120bf
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-08-22 05:55:21 +0300

    two day

---
+++ two day application.py
@@ -0,0 +1,14 @@
+#this is a  comment
+print("Hello, world!")
+
+print("Hello, world!") #this is a comment
+
+#print("Hello, world!")
+print("Cheers, Mate!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, world!")
