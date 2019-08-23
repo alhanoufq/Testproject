3ec00a46267cd973ebf99e155b3e52995c3079ce
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-08-23 05:34:23 +0300

    add 3 day

---
+++ 3 day application.py
@@ -0,0 +1,37 @@
+x=5
+y="John"
+print(x)
+print(y)
+
+x=4 #x is of type int
+x="Sally" #x is now of type str
+print(x)
+
+x="John"
+print(x)
+#double quotes are the same as single quotes
+x='John'
+print(x)
+
+x,y,z="Orange","Banana","Cherry"
+print(x)
+print(y)
+print(z)
+
+name1=name2=name3="Tomayto"
+print(name1)
+print(name2)
+print(name3)
+
+
+B = "awesome"
+print("Python is "+ B)
+
+x="Python is "
+y="Programming"
+z=x+y
+print(z)
+
+x=5
+y=10
+print(x+y)
