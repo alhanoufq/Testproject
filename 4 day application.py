8bcbdf358e176d4e6c1ce3619e692d3a61ace7d5
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-08-25 01:40:37 +0300

    4 day add

---
+++ 4 day application.py
@@ -0,0 +1,30 @@
+x=6    #int
+print(type(x))
+
+y=5.8  #float
+print(type(y))
+
+z=2j   #complex
+print(type(z))
+
+J=53391188356
+print(type(J))
+
+x=45+6j
+print(type(x))
+
+x=1   #int
+y=4.2 #float
+z=-5j #complex
+a=float(x)
+b=int(y)
+c=complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+import random
+print(random.randrange(1,10))
