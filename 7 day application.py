62c2ec6ce5494cbfc883a1912f168256874fb483
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-08-28 00:01:45 +0300

    7 add

---
+++ 7 day application.py
@@ -0,0 +1,16 @@
+print('Hello')
+print("Hello")
+
+a="Hello"
+print(a)
+
+a=''' Never give up on a dream just because of the time it will take to accomplish it.
+      The time will pass anyway.” — Earl Nightingale '''
+print(a)
+
+a="Hello, world!"
+print(a[1])
+
+b="Hello, world!"
+print(b[2:5])
+print(b[1:4])
