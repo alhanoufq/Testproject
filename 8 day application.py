ee952a954298a17e178e440b44ec6e947791900e
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-08-31 05:09:16 +0300

    8

---
+++ 8 day application.py
@@ -0,0 +1,26 @@
+a="Hello, World!"
+#return Hello World
+print(a.strip())
+
+a="Hello, World!"
+print(len(a))
+
+a="HELLO, PRINESS!"
+print(a.lower())
+
+a="hello, priness!"
+print(a.upper())
+
+
+z="Hello, World!"
+print(z.replace("H","J"))
+
+x="Hello, World!"
+print(x.split(","))
+
+
+
+
+
+
+
