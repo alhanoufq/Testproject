aefabff9956c637418caf75ce0387e59f0b8b759
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-09-15 02:25:53 +0300

    add day 10

---
+++ 10 day application.py
@@ -0,0 +1,29 @@
+#used Multiplication are Arithmetic operators
+x=5
+y=3
+print(x*y)
+
+#used Floor division are Arithmetic operators
+x=9
+y=5
+print(x//y)
+
+#used Modulus are Arithmetic operators
+x=34
+y=15
+print(x%y)
+
+#used Assignment operators
+x<<=5
+y/=4
+print(x)
+print(y)
+
+#used Comparison operators
+x=6
+y=4
+print(x>=y)
+
+x=63
+y=24
+print(x==y)
