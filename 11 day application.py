413647eb3502c7a772d3827d9e4c5331597b9bff
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-09-15 23:56:35 +0300

    11 day add

---
+++ 11 day application.py
@@ -0,0 +1,20 @@
+#used logical operators
+x=7
+print(x>4 or x<=5)
+print(x>8 and x<5)
+print(not(x>=6 and x<4))
+
+#used identity operators
+x=["Ahmad","Rashed"]
+y=["Ahmad","Rashed"]
+z=y
+print(x is not y)
+print(y is not z)
+print(y !=z)
+
+#used Memebershop operators
+x=["green","red"]
+y=["green","red","black"]
+print("red" in x)
+print("black" in x)
+
