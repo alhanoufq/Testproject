2c31a62919047aae29b035dea848ef2111103e69
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-09-21 23:42:31 +0300

    13

---
+++ 13 day application.py
@@ -0,0 +1,38 @@
+#integer list
+numbers = [1,2,3,4,5]
+print(numbers)
+
+#string list
+thislist = ["apple","banana","cherry"]
+print(thislist)
+
+#string and integer list
+thislist = ["apple","banana","cherry",1,2,3]
+print(thislist)
+
+#Decimal numbers list
+thislist = [3.4,5.2,1.5,0.7]
+print(thislist)
+
+#Access Items list
+thislist = ["apple","banana","cherry"]
+print(thislist[0])
+
+#Loop Through a List
+numberslist = [67,34,97]
+for x in numberslist:
+    print(x)
+
+#Change Item Value
+thislist = ["apple","banana","cherry"]
+thislist[1] ="blackcurrent"
+print(thislist)
+
+#delete item
+thislist = ["A","B","C","D","F","G","M"]
+del thislist[0]
+del thislist[4]
+del thislist[2]
+print(thislist)
+
+
