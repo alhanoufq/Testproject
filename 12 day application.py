6045999e23513e8813d79b3262888dc5e713a63a
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-09-21 04:44:28 +0300

    12

---
+++ 12 day application.py
@@ -0,0 +1,14 @@
+
+#1
+txt= "Please, I want {} sandwishes and {} donutes"
+print(txt.replace("I","we"))
+
+#2
+quantity=5
+itemno=7
+txt= "Please, I want {} sandwishes and {} donutes"
+print(txt.format(quantity,itemno))
+
+#3
+txt= "Please, I want {} sandwishes and {} donutes"
+print(txt.upper())
