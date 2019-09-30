565206de8a4fc241145b6e527eee705982c96588
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-10-01 01:49:16 +0300

    16

---
+++ 16 day application.py
@@ -0,0 +1,25 @@
+#Create tuple
+Thistuple = ("Noha","salma","sara","nada")
+print(Thistuple)
+#empty tuple
+Thistuple = ()
+print(Thistuple)
+#one items
+Thistuple = (4,)
+print(Thistuple)
+#multiple items
+Thistuple = (4,2.3,5,4.9)
+print(Thistuple)
+#tuple one or more of data types
+Thistuple = ("Ethar",8,3.6,"اهلن")
+print(Thistuple)
+#ِAccess tuple items
+numbers =(2,7,3,9,5,2)
+print(numbers[3])
+#Loop Through a Tuple
+string =("Aa","Cc","Tt","Jj")
+for x in string:
+     print(x)
+#tuple part
+num =("huda","nada","rahaf","sara","wajdan")
+print(num[1:4])
