98b166b6a5b94f6af4722bd094835dd601abd967
Alhanouf <Alhanouf.techcampus@gmail.com>
2019-11-24 21:08:43 +0300

    29

--- 28 day application.py
+++ 28 day application.py
@@ -27,3 +27,4 @@ while i<6:
 else:
     print("i is no longer less than 6")
 
+
---
+++ 29 day application.py
@@ -0,0 +1,24 @@
+# The For Loops
+fruits=['apple','banana','cherry']
+for x in fruits:
+    print(x)
+
+#Looping Through a String
+fruits=['apple','banana','cherry']
+for x in 'apple':
+    print(x)
+
+#the break
+fruits=['apple','banana','cherry']
+for x in fruits:
+    print(x)
+    if x=='banana':
+     break
+
+#The continue Statement
+fruits=['apple','banana','cherry']
+for x in fruits:
+    if x=='banana':
+     continue
+    print(x)
+    
